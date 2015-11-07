d38cb59ead5bf51f0b18365b85e7b04825b2bcc2
JDaveWallis <dave.wallis@insperity.com>
2015-11-07 16:21:19 -0600

    Exploratory Data Analysis - assignment 1, plot recreation

---
+++ plot1.R
@@ -0,0 +1,63 @@
+### Exploratory Data Analysis - Course Project #1 - PLOT #1
+### Submitted by Dave Wallis, 11/7/2015
+###
+### Note - The source data was processed a single time, using the R code script
+### called "ProcessSelectedUCIData.R" - that file is included in the Git library.
+### The steps of that process are included in each of the plot#.R
+### scripts, in order to meet the requirements of the course assignment.
+### But the intent is that the source processing only needs to be run one time
+### before the plots are generated.
+###
+###
+### "ProcessSelectedUCIData.R"
+###
+### Read through the entire raw UCI Electric Power Consumption dataset.
+### This step is expensive, but will only need to be done one time to
+### produce the subsetted selected data from 2/1/2007 and 2/2/2007.
+### Load into the RawData dataframe.
+### (specifying the semicolon as the separator, including header from the file,
+### and specifying stringsAsFactors=FALSE so that character columns are read as
+### character data - not factors).
+###
+
+#RawData <- data.frame(read.table(file="household_power_consumption.txt",
+#                                 header=TRUE,sep=';',
+#                                 stringsAsFactors=FALSE))
+
+### Use the filter() function of the "dplyr" package to extract those records where
+### the first column has values of 1/2/2007 or 2/2/2007 (where the date format is DD/MM/YYYY,
+### with single digit months and days not having leading zeroes.)
+###
+
+#library(dplyr)
+#
+#SelectData <- filter(RawData,Date=="1/2/2007"|Date=="2/2/2007")
+
+### Write the selected subset of data to a working file.
+###
+
+#write.table(SelectData, file="SelectData.txt")
+
+### After the initial data processing, only this selected subset data file (SelectData.txt)
+### is required - we don't have to perform the expensive initial select any more to meet the
+### requirements of the assignment.
+###
+
+PlotData <- data.frame(read.table(file="SelectData.txt",stringsAsFactors=FALSE))
+PlotData$Time <- as.POSIXct(paste(PlotData$Date,PlotData$Time), format="%d/%m/%Y %H:%M:%S")
+PlotData$Date <- as.Date(PlotData$Date,"%d/%m/%Y")
+
+
+### Code to produce the individual plots for the assignment
+###
+### Plot #1 - histogram of frequency of Global Active Power values
+###
+png(filename = "plot1.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white")
+
+hist(PlotData$Global_active_power,
+     col="red", 
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
+
---
+++ plot2.R
@@ -0,0 +1,65 @@
+### Exploratory Data Analysis - Course Project #1 - PLOT #2
+### Submitted by Dave Wallis, 11/7/2015
+###
+### Note - The source data was processed a single time, using the R code script
+### called "ProcessSelectedUCIData.R" - that file is included in the Git library.
+### The steps of that process are included in each of the plot#.R
+### scripts, in order to meet the requirements of the course assignment.
+### But the intent is that the source processing only needs to be run one time
+### before the plots are generated.
+###
+###
+### "ProcessSelectedUCIData.R"
+###
+### Read through the entire raw UCI Electric Power Consumption dataset.
+### This step is expensive, but will only need to be done one time to
+### produce the subsetted selected data from 2/1/2007 and 2/2/2007.
+### Load into the RawData dataframe.
+### (specifying the semicolon as the separator, including header from the file,
+### and specifying stringsAsFactors=FALSE so that character columns are read as
+### character data - not factors).
+###
+
+#RawData <- data.frame(read.table(file="household_power_consumption.txt",
+#                                 header=TRUE,sep=';',
+#                                 stringsAsFactors=FALSE))
+
+### Use the filter() function of the "dplyr" package to extract those records where
+### the first column has values of 1/2/2007 or 2/2/2007 (where the date format is DD/MM/YYYY,
+### with single digit months and days not having leading zeroes.)
+###
+
+#library(dplyr)
+#
+#SelectData <- filter(RawData,Date=="1/2/2007"|Date=="2/2/2007")
+
+### Write the selected subset of data to a working file.
+###
+
+#write.table(SelectData, file="SelectData.txt")
+
+### After the initial data processing, only this selected subset data file (SelectData.txt)
+### is required - we don't have to perform the expensive initial select any more to meet the
+### requirements of the assignment.
+###
+
+PlotData <- data.frame(read.table(file="SelectData.txt",stringsAsFactors=FALSE))
+PlotData$Time <- as.POSIXct(paste(PlotData$Date,PlotData$Time), format="%d/%m/%Y %H:%M:%S")
+PlotData$Date <- as.Date(PlotData$Date,"%d/%m/%Y")
+
+
+### Code to produce the individual plots for the assignment
+###
+### Plot #2 - line diagram of Global Active Power values across 2 days by minute
+###
+png(filename = "plot2.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white")
+plot(PlotData$Time,PlotData$Global_active_power,
+     type="l",
+     col="black", 
+     fg="black",
+     main="Global Active Power by Minute",
+     xlab="",
+     ylab="Global Active Power (kilowatts)")
+
